61ea92a7be64df33aee74c7c62d0e5113fa4f8aa
coffeecode <35342238+0xC0FFEEC0DE@users.noreply.github.com>
2019-11-15 17:41:06 +0200

    urls

--- instagram-likes-back.user.js
+++ instagram-likes-back.user.js
@@ -1,8 +1,12 @@
 // ==UserScript==
 // @name         Instagram Likes Back
 // @namespace    instagram-likes-back
-// @version      0.0.1
+// @version      0.0.2
 // @description  See likes in Instagram again
+// @homepageURL  https://github.com/0xC0FFEEC0DE/instagram-likes-back
+// @supportURL   https://github.com/0xC0FFEEC0DE/instagram-likes-back/issues
+// @downloadURL  https://raw.githubusercontent.com/0xC0FFEEC0DE/instagram-likes-back/master/instagram-likes-back.user.js
+// @updateURL    https://raw.githubusercontent.com/0xC0FFEEC0DE/instagram-likes-back/master/instagram-likes-back.user.js
 // @author       0xC0FFEEC0DE
 // @include      https://*.instagram.com/*
 // @license      MIT
