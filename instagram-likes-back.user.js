d51f741bd3a200149909724cc0d14c9f5bae26fd
0xdv <0xc0ffec0de@gmail.com>
2019-11-17 22:48:59 +0200

    links

--- instagram-likes-back.user.js
+++ instagram-likes-back.user.js
@@ -3,10 +3,10 @@
 // @namespace    instagram-likes-back
 // @version      0.0.5
 // @description  See likes in Instagram again
-// @homepageURL  https://github.com/0xC0FFEEC0DE/instagram-likes-back
-// @supportURL   https://github.com/0xC0FFEEC0DE/instagram-likes-back/issues
-// @downloadURL  https://raw.githubusercontent.com/0xC0FFEEC0DE/instagram-likes-back/master/instagram-likes-back.user.js
-// @updateURL    https://raw.githubusercontent.com/0xC0FFEEC0DE/instagram-likes-back/master/instagram-likes-back.user.js
+// @homepageURL  https://github.com/0xdv/instagram-likes-back
+// @supportURL   https://github.com/0xdv/instagram-likes-back/issues
+// @downloadURL  https://raw.githubusercontent.com/0xdv/instagram-likes-back/master/instagram-likes-back.user.js
+// @updateURL    https://raw.githubusercontent.com/0xdv/instagram-likes-back/master/instagram-likes-back.user.js
 // @author       0xC0FFEEC0DE
 // @include      https://*.instagram.com/*
 // @license      MIT
